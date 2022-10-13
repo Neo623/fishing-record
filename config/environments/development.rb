412247b4d453b5b2f23dcf0c1efb2785901c6fe5
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-10-13 13:28:27 +0000

    [Add]environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "8286478a1db6463a9c99ee1f210f8a66.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
