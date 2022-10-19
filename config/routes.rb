847242d59ebbfae1101b7d2a74ffd859cb8a49b2
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-10-19 14:26:59 +0000

    [Add]hedaer編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about"
 
-  resources :posts, only: [:index,:show,:edit,:create,:destroy,:update] do
+  resources :posts, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
     resource :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 
-  resources :users, only: [:index,:show,:edit,:update]
+   get "my_page" => "users#show"
+   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
